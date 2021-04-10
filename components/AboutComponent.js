732eca11aa7a692b6eb9c1c85291120a581ccb70
Tyler Pritchard <pritchard.tyler@gmail.com>
2021-04-10 09:39:07 -0700

    Complete Task 3

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import {Card, Text, ListItem} from 'react-native-elements';
+import {ScrollView, FlatList} from 'react-native';
+import {PARTNERS} from '../shared/partners';
 
-import {ScrollView} from 'react-native';
 
+function Mission() {
+
+    return (
+        <Card>
+            <Text style={{margin: 10}}>
+            We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );
+}
 class About extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            partners: PARTNERS
         };
     }
 
@@ -14,9 +27,31 @@ class About extends Component {
     }
 
     render() {
+
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('../shared/images/bootstrap-logo.png')}}
+                />
+            );
+        };
+
         return(
             <ScrollView>
-                
+               <Mission />
+               <Card
+                title="Community Partners"
+               >
+                   <FlatList
+                        data={this.state.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                   >
+
+                   </FlatList>
+                </Card> 
             </ScrollView>
         );
     }
